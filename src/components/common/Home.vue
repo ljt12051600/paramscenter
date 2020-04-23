33cfdcfa8c5f6abcfab5f359dcdaad2c3d732584
jacksyang <1315310845@qq.com>
2020-04-23 14:43:06 +0800

    修改页面

--- src/components/common/Home.vue
+++ src/components/common/Home.vue
@@ -13,7 +13,7 @@
                 <el-backtop target=".content"></el-backtop>
             </div>
         </div>
-      
+
     </div>
 </template>
 
@@ -56,136 +56,138 @@
         },
         methods: {
             async init() {
-                setTimeout(() => {
+              
                     let data = {
-                    name: " 杨建忠",
-                    items: [{
-                        icon: 'el-icon-lx-home',
-                        index: 'dashboard',
-                        title: '系统首页'
-                    },
-                    {
-                        icon: 'el-icon-lx-cascades',
-                        index: 'table',
-                        title: '基础表格'
-                    },
-                    // {
-                    //     icon: 'el-icon-lx-copy',
-                    //     index: 'tabs',
-                    //     title: 'tab选项卡'
-                    // },
-                    {
-                        icon: 'el-icon-lx-calendar',
-                        index: '3',
-                        title: '表单相关',
-                        subs: [{
-                            index: 'form',
-                            title: '基本表单'
+                        name: " 杨建忠",
+                        items: [{
+                            icon: 'el-icon-lx-home',
+                            index: 'dashboard',
+                            title: '系统首页'
                         },
                         {
-                            index: '3-2',
-                            title: '三级菜单',
+                            icon: 'el-icon-lx-cascades',
+                            index: 'table',
+                            title: '基础表格'
+                        },
+                        // {
+                        //     icon: 'el-icon-lx-copy',
+                        //     index: 'tabs',
+                        //     title: 'tab选项卡'
+                        // },
+                        {
+                            icon: 'el-icon-lx-calendar',
+                            index: '3',
+                            title: '表单相关',
                             subs: [{
-                                index: 'editor',
-                                title: '富文本编辑器'
+                                index: 'form',
+                                title: '基本表单'
                             },
                             {
-                                index: 'markdown',
-                                title: 'markdown编辑器'
+                                index: '3-2',
+                                title: '三级菜单',
+                                subs: [{
+                                    index: 'editor',
+                                    title: '富文本编辑器'
+                                },
+                                {
+                                    index: 'markdown',
+                                    title: 'markdown编辑器'
+                                }
+                                ]
+                            },
+                            {
+                                index: 'upload',
+                                title: '文件上传'
                             }
                             ]
                         },
                         {
-                            index: 'upload',
-                            title: '文件上传'
-                        }
-                        ]
-                    },
-                    {
-                        icon: 'el-icon-lx-emoji',
-                        index: 'icon',
-                        title: '自定义图标'
-                    },
-                    {
-                        icon: 'el-icon-pie-chart',
-                        index: 'charts',
-                        title: 'schart图表'
-                    },
-                    {
-                        icon: 'el-icon-rank',
-                        index: '6',
-                        title: '拖拽组件',
-                        subs: [{
-                            index: 'drag',
-                            title: '拖拽列表'
+                            icon: 'el-icon-lx-emoji',
+                            index: 'icon',
+                            title: '自定义图标'
                         },
                         {
-                            index: 'dialog',
-                            title: '拖拽弹框'
-                        }
-                        ]
-                    },
-                    {
-                        icon: 'el-icon-lx-global',
-                        index: 'i18n',
-                        title: '国际化功能'
-                    },
-                    {
-                        icon: 'el-icon-lx-warn',
-                        index: '7',
-                        title: '错误处理',
-                        subs: [{
-                            index: 'permission',
-                            title: '权限测试'
+                            icon: 'el-icon-pie-chart',
+                            index: 'charts',
+                            title: 'schart图表'
                         },
                         {
-                            index: '404',
-                            title: '404页面'
-                        }
+                            icon: 'el-icon-rank',
+                            index: '6',
+                            title: '拖拽组件',
+                            subs: [{
+                                index: 'drag',
+                                title: '拖拽列表'
+                            },
+                            {
+                                index: 'dialog',
+                                title: '拖拽弹框'
+                            }
+                            ]
+                        },
+                        {
+                            icon: 'el-icon-lx-global',
+                            index: 'i18n',
+                            title: '国际化功能'
+                        },
+                        {
+                            icon: 'el-icon-lx-warn',
+                            index: '7',
+                            title: '错误处理',
+                            subs: [{
+                                index: 'permission',
+                                title: '权限测试'
+                            },
+                            {
+                                index: '404',
+                                title: '404页面'
+                            }
+                            ]
+                        },
                         ]
-                    },
 
-
-
-
-
-
-                    ]
-
-                }
-                this.updateMainInfo(data);
-
-                
-                
-
-
-
-                router.beforeEach((to, from, next) => {
-                    if (to.path == "403" || to.path == "404") {
-                        next();
                     }
+                    let menuArr = [];
 
+                    ; (function getMenuArr(Arr) {
+                        if (Arr.length) {
+                            Arr.forEach(item => {
+                                if (item.subs && item.subs.length > 0) {
+                                    getMenuArr(item.subs)
+                                }
 
-                    else {
+                                else { menuArr.push(item.index) }
 
-                        // 简单的判断IE10及以下不进入富文本编辑器，该组件不兼容
-                        if (navigator.userAgent.indexOf('MSIE') > -1 && to.path === '/editor') {
-                            Vue.prototype.$alert('vue-quill-editor组件不兼容IE10及以下浏览器，请使用更高版本的浏览器查看', '浏览器不兼容通知', {
-                                confirmButtonText: '确定'
-                            });
-                        } else {
+                            })
+
+                        }
+                    })(data.items)
+                    console.log(menuArr);
+                    this.updateMainInfo(data);
+
+                    router.beforeEach((to, from, next) => {
+                       
+                        console.log(to.path)
+                        if (to.path == "/403" || to.path == "/404"||to.path=="/login") {
+                           
                             next();
                         }
-                    }
+                        else if(!menuArr.includes(to.path.substr(1))){
+                           
+                            next("/403")
 
-
-                    // 简单的判断IE10及以下不进入富文本编辑器，该组件不兼容
-
-
-                });
-
-                }, 20000);
+                        }
             
+                        else {
+                            console.log(3)
+                            next();
+                           
+                        }
+
+
+                    });
+
+
             },
             ...mapMutations(["updateMainInfo"])
         }
--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,9 @@ import Router from 'vue-router';
 
 
 Vue.use(Router);
-let Router1
 
- Router1= new Router({
+
+export default new Router({
     routes: [
         {
             path: '/',
@@ -113,22 +113,5 @@ let Router1
         }
     ]
 });
-Router1.beforeEach((to, from, next) => {
-   
-    const role = localStorage.getItem('ms_username');
-    if (!role && to.path !== '/login') {
-        next('/login');
-        
-    } else {
-      
-        // 简单的判断IE10及以下不进入富文本编辑器，该组件不兼容
-        if (navigator.userAgent.indexOf('MSIE') > -1 && to.path === '/editor') {
-            Vue.prototype.$alert('vue-quill-editor组件不兼容IE10及以下浏览器，请使用更高版本的浏览器查看', '浏览器不兼容通知', {
-                confirmButtonText: '确定'
-            });
-        } else {
-                next();
-        }
-    }
-});
-export default Router1
+
+
