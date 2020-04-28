b23e9c359064347cfdba6ee5f69fa9f795eea7d3
jacksyang <1315310845@qq.com>
2020-04-28 17:12:13 +0800

    修改问题

--- src/components/page/system/menu/menu.vue
+++ src/components/page/system/menu/menu.vue
@@ -7,7 +7,8 @@
                     <div slot="header" class="clearfix">
                         <span>菜单结构</span>
                     </div>
-                    <el-tree :data="TreeData">
+                    <el-tree @node-click="nodeClick" highlight-current accordion :data="TreeData">
+
                     </el-tree>
                 </el-card>
             </el-col>
@@ -17,24 +18,29 @@
                 <el-card class="min-height">
                     <div slot="header" class="clearfix">
                         <span>菜单内容</span>
-
-                        <el-button @click="edit" size="big" style="float: right; padding: 3px 0;width: 100px;height: 30px;margin-left:20px;" type="primary">
-                            <i class="el-icon-edit"></i>修改</el-button>
-                        <el-button size="big" style="float: right; padding: 3px 0;width: 100px;height: 30px;" type="danger">
+                        <el-button v-if="showDelete" size="big"
+                            style="float: right; padding: 3px 0;width: 100px;height: 30px;margin-left:20px;"
+                            type="danger">
                             <i class="el-icon-lx-redpacket_fill"></i>删除菜单</el-button>
-                        <el-button size="big" style="float: right; padding: 3px 0;width: 100px;height: 30px;" type="primary">
+
+                        <el-button @click="edit" size="big"
+                            style="float: right; padding: 3px 0;width: 100px;height: 30px;margin-left:20px;"
+                            type="warning">
+                            <i class="el-icon-edit"></i>修改</el-button>
+
+                        <el-button size="big"
+                            style="float: right; padding: 3px 0;width: 100px;height: 30px;margin-left:20px;"
+                            type="primary">
                             <i class="el-icon-plus"></i>新增子菜单</el-button>
-                        
+
 
                     </div>
-                    {{postObj}}
-                    <el-form :rules="rules" :model="postObj" ref="form" label-width="100px">
+
+                    <el-form :disabled="formDisabled" :rules="rules" :model="postObj" ref="form" label-width="100px">
                         <el-row :gutter="20">
                             <el-col align="left" :span="12">
-                                <el-form-item label="父级菜单" >
+                                <el-form-item label="父级菜单">
                                     根目录
-                                    
-
                                 </el-form-item>
                             </el-col>
                             <el-col align="left" :span="12">
@@ -57,12 +63,12 @@
                                     <el-input style="width:200px;" v-model.trim="postObj.name"></el-input>
                                 </el-form-item>
                             </el-col>
-                            <el-col align="left" :span="12">
+                            <el-col v-if="postObj.menuType=='L'" align="left" :span="12">
                                 <el-form-item label="流程编号">
                                     <el-input style="width:200px;" v-model.trim="postObj.flowKey"></el-input>
                                 </el-form-item>
                             </el-col>
-                            <el-col align="left" :span="12">
+                            <el-col v-if="postObj.menuType=='L'" align="left" :span="12">
                                 <el-form-item label="版本管理">
                                     <el-select style="width:200px;" v-model="postObj.isEdition" clearable
                                         placeholder="请选择">
@@ -71,23 +77,24 @@
                                     </el-select>
                                 </el-form-item>
                             </el-col>
-                            <el-col align="left" :span="12">
+                            <el-col v-if="postObj.menuType=='L'" align="left" :span="12">
                                 <el-form-item label="交易名称">
                                     <el-input style="width:200px;" v-model.trim="postObj.tranName"></el-input>
                                 </el-form-item>
                             </el-col>
-                            <el-col align="left" :span="12">
+                            <el-col v-if="postObj.menuType=='L'" align="left" :span="12">
                                 <el-form-item label="菜单URL">
                                     <el-input style="width:200px;" v-model.trim="postObj.menuUrl"></el-input>
                                 </el-form-item>
                             </el-col>
 
                         </el-row>
-                        <el-transfer :props="{
+                        {{postObj.buttonPerms}}
+                        <el-transfer v-if="postObj.menuType=='L'" :props="{
                             key: 'buttonId',
                             label: 'buttonName'
-                          }" style="margin-left:100px;" filterable v-model="value" :titles="['可选按钮', '已选按钮']"
-                            :data="allButtons">
+                          }" style="margin-left:100px;" filterable v-model="buttonPerms"
+                            :titles="['可选按钮', '已选按钮']" :data="allButtons">
 
                         </el-transfer>
 
@@ -106,25 +113,9 @@
 </template>
 
 <script>
-    function unitTree(arr) {
-        let newArr = [];
-        let Arr = deleteKey(arr);
 
-        Arr.forEach(item => {
-            item.label = item.name;
-            console.log(item.pId)
-            if (item.pId === "0") {
-                // alert(1);
-
-                newArr.push(item);
-            }
-        });
-        console.log(newArr);
-
-
-    }
     import { deleteKey } from "@/utils"
-    import { queryMenu, querySysButtonsListAll, updateMenu,queryMenuForTree,queryMenuListByUserFuncId } from "@/api/system"
+    import { queryMenu, querySysButtonsListAll, updateMenu, queryMenuForTree } from "@/api/system"
     export default {
         data() {
             return {
@@ -134,8 +125,12 @@
                     key: 'success',
                     label: "123",
                 }],
+
+                showDelete: false,
+                formDisabled: true,
                 postObj: {},
                 allButtons: [],
+                buttonPerms:[],
                 rules: {
                     menuType: [
                         { required: true, message: '请输入菜单类型', trigger: 'change' },
@@ -152,27 +147,62 @@
 
             }
         },
-
         methods: {
             async init() {
                 let info = await queryMenuForTree();
                 if (info.resCode === "0") {
-                    let TreeData = info.rows;
-                    unitTree(TreeData)
-
-                    // console.log(this.TreeData)
-
+                    this.TreeData = info.rows;
                 }
                 let info_button = await querySysButtonsListAll();
                 if (info_button.resCode === "0") {
                     this.allButtons = info_button.rows;
                 }
-                let info_button1 = await queryMenuListByUserFuncId();
-              
 
             },
-            edit(){
-                console.log(this.$refs['form'].resetField)
+            edit() {
+               this.formDisabled=false;
+            },
+            nodeClick(item, index) {
+                this.buttonPerms=[];
+
+                this.postObj = {
+                    id:item.id,
+                    pId:item.pId,
+                    name:item.name,
+                    menuId:item.menuId,
+                    menuType:item.menuType
+                }
+                
+                if (item.menuType == 'L') {
+                    this.postObj.flowKey = item.flowKey;
+                    this.postObj.isEdition = item.isEdition;
+                    this.postObj.tranName = item.tranName;
+                    this.postObj.menuUrl = item.menuUrl;
+
+
+                    if (!item.buttonPerms) {
+                        this.buttonPerms = [];
+
+                    }
+                    else {
+                        this.buttonPerms = item.buttonPerms.split(",")
+
+                    }
+                 
+
+                   
+                }
+
+
+
+
+                if (item.children && item.children.length > 0) {
+                    this.showDelete = false;
+                }
+                else {
+                    this.showDelete = true;
+                }
+
             }
 
 
