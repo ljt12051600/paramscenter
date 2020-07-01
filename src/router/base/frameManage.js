64569da15e184b44a79d36861a95a123763cff6c
lhhgitt <1282425174@qq.com>
2020-07-01 16:12:07 +0800

    架构管控

--- src/router/base/frameManage.js
+++ src/router/base/frameManage.js
@@ -3,22 +3,22 @@ const FRAMEMANAGEROUTER=[
     {
         path: '/sysDefine',
         component: () => import(/* webpackChunkName: "frameManage" */ "@page/frameManage/sysDefine/list.vue"),
-        meta: { title: '系统定义2.0' }
+        meta: { title: '系统定义' }
     },
     {
         path: '/subSysDefine',
         component: () => import(/* webpackChunkName: "frameManage" */ "@page/frameManage/subSysDefine/list.vue"),
-        meta: { title: '子系统定义2.0' }
+        meta: { title: '子系统定义' }
     },
     {
         path: '/subdomain',
         component: () => import(/* webpackChunkName: "frameManage" */ "@page/frameManage/subdomain/list.vue"),
-        meta: { title: '子域2.0' }
+        meta: { title: '子域' }
     },
     {
         path: '/tpsArOsItf',
         component: () => import(/* webpackChunkName: "frameManage" */ "@page/frameManage/tpsArOsItf/list.vue"),
-        meta: { title: '外联系统接口2.0' }
+        meta: { title: '外联系统接口' }
     },
 
 ]
