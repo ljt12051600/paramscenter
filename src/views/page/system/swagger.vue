5c0f2a0ddf9578fcf9ea14b7835293132a9a5fb7
jacksyang <513980580@qq.com>
2020-08-19 11:17:22 +0800

    xx

--- src/views/page/system/swagger.vue
+++ src/views/page/system/swagger.vue
@@ -1,5 +1,5 @@
 <template>
-  <div  style="width:100%;height:92%">
+  <div  style="width:100%;height:100%">
 
        <iframe style="width:100%;height:100%" :src="url" frameborder="0"></iframe>
       
